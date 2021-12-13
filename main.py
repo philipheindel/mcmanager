6caef2fd962086a493b8d6f5be950ac060f3bf3a
Philip Heindel <p.e.heindel@gmail.com>
2021-12-13 12:35:51 +0000

    Added more static and template files and restructured main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+import configparser
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask import make_response
-from subprocess import call
-import json
 from utils.Mcserver import Mcserver
 from utils.Config import Config
+from jproperties import Properties
 
-config = Config()
-config.load_mcmanager()
-config.load_mcserver()
-config.load_minecraft()
+#region Load Configs
 
-mcserver = Mcserver("/home/philip/mcserver/mcserver")
+config_loader = Config()
+mcmanager_config = config_loader.load_mcmanager()
+mcserver_config = config_loader.load_mcserver()
+minecraft_config = config_loader.load_minecraft()
+
+#endregion
+
+#region File Path Assembly
+
+root_directory = mcserver_config["root_directory"]
+mcserver_script = Mcserver(root_directory + mcserver_config["mcserver"])
+serverfiles_directory = root_directory + mcserver_config["serverfiles"]
+server_properties_file = serverfiles_directory + mcserver_config["server_properties"]
+whitelist_file = serverfiles_directory + mcserver_config["whitelist"]
+ops_file = serverfiles_directory + mcserver_config["ops"]
+banned_ips_file = serverfiles_directory + mcserver_config["banned_ips"]
+banned_players_file = serverfiles_directory + mcserver_config["banned_players"]
+print(server_properties_file)
+p = Properties()
+with open(server_properties_file, "rb") as f:
+    p.load(f)
+#endregion
+
+#region Flask Initialization
 
 app = Flask(
     __name__,
@@ -20,29 +40,74 @@ app = Flask(
     static_folder='static'
 )
 
-@app.route("/")
-def home():
-    
+#endregion
 
-    f = open('/home/philip/mcserver/serverfiles/whitelist.json')
-    data = json.load(f)
-    whitelist = data
+#region Page Endpoints
+
+@app.route("/")
+def main():
+    page = "main"
+    
+    config = configparser.ConfigParser()
+    config.read(server_properties_file)
+    
+    print(config)
+
+    response = make_response(
+        render_template(
+            page + ".html",
+            page=page
+        )
+    )
+    #response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route("/players")
+def players():
+    page = "players"
+    
+    whitelist = config_loader.load_json(whitelist_file)
     
     response = make_response(
         render_template(
-            "main.html",
+            page+".html",
+            page="players",
             whitelist=whitelist
         )
     )
-    
-    response.headers.add("Access-Control-Allow-Origin", "*")
-
     return response
+
+@app.route("/world")
+def world():
+    page = "world"
+    
+    response = make_response(
+        render_template(
+            page+".html",
+            page=page
+        )
+    )
+    return response
+
+@app.route("/server")
+def server():
+    
+    return 200
+
+#endregion
+
+#region API Endpoints
 
 @app.route("/execute/<command>", methods = ["POST"])
 def execute(command):
-    print(command)
+    try:
+        print(command)
+        mcserver_script
+    except BaseException as err:
+        print(err)
     return "Ran " + command, 200
+
+
 
 @app.route("/time", methods = ["POST"])
 def time():
@@ -50,7 +115,7 @@ def time():
         data = request.json
         action = data["action"]
         time = data["time"]
-        mcserver.time(action, time)
+        mcserver_script.time(action, time)
 
         response = make_response(
             200
@@ -66,13 +131,18 @@ def weather():
     data = request.json
     weather = data["weather"]
     
-    mcserver.weather(weather)
+    mcserver_script.weather(weather)
     
     return "set weather to clear", 200
     
+#endregion
+
+#region Run Flask
 
 if __name__ == "__main__":
     app.run(
         debug=True,
         host='0.0.0.0'
     )
+    
+#endregion
--- utils/Config.py
+++ utils/Config.py
@@ -1,37 +1,18 @@
 import json
 
 class Config():
-    MCMANAGER_FILE = "./config/mcmanager.json"
-    MCSERVER_FILE = "./config/mcserver.json"
-    MINECRAFT_FILE = "./config/minecraft.json"
     
-    mcmanager_config = {}
-    mcserver_config = {}
-    minecraft_config = {}
-    
-    def load_mcmanager(self):
-        self.load_json(self.MCMANAGER_FILE)
+    def load_mcmanager(self, mcmanager_file="./config/mcmanager.json"):
+        return self.load_json(mcmanager_file)
         
-    def load_mcserver(self):
-        self.load_json(self.MCSERVER_FILE)
+    def load_mcserver(self, mcserver_file="./config/mcserver.json"):
+        return self.load_json(mcserver_file)
         
-    def load_minecraft(self):
-        self.load_json(self.MINECRAFT_FILE)
+    def load_minecraft(self, minecraft_file="./config/minecraft.json"):
+        return self.load_json(minecraft_file)
         
     def load_json(self, file):
         opened_file = open(file)
         loaded_data = json.load(opened_file)
         return loaded_data
         
-        
-
-def load_config():
-    opened_file = open("./config/mcserver.json")
-
-    data = json.load(opened_file)
-    
-    print(data["commands"]["lgsm"][""])
-
-    return data
-
-
