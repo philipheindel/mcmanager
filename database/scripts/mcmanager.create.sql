03da712a6ed0803cf6ce691ce893554d0b460f9d
Philip Heindel <p.e.heindel@gmail.com>
2021-12-17 16:38:03 +0000

    Added stub of table creation sql script

--- database/scripts/mcmanager.create.sql
+++ database/scripts/mcmanager.create.sql
@@ -4,3 +4,29 @@ CREATE TABLE Audit (
 	Message TEXT NOT NULL,
 	System TEXT NOT NULL
 );
+
+CREATE TABLE Players (
+	UUID TEXT PRIMARY KEY NOT NULL,
+	Username TEXT NOT NULL,
+	Name TEXT,
+	Invitee, TEXT,
+	DateAdded DATETIME,
+	Notes TEXT
+);
+
+INSERT INTO Players (
+	UUID,
+	Username,
+	Name,
+	Invitee,
+	DateAdded,
+	Notes
+)
+VALUES (
+	"19229106-a9f4-4e90-8635-6c5a5ea70f42",
+	"RedwardFlip",
+	"Philip",
+	"Founder",
+	"2021-11-30 19:00:00",
+	"Stuff"
+);
